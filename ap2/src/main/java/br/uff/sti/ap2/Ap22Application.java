7813b56b3fd079718b6e8cc828eca78280b42954
JoaoLopez <joao.p.lopez01@gmail.com>
2026-04-20 01:15:26 +0000

    E2: Implementando método main

--- ap2/src/main/java/br/uff/sti/ap2/Ap22Application.java
+++ ap2/src/main/java/br/uff/sti/ap2/Ap22Application.java
@@ -1,5 +1,7 @@
 package br.uff.sti.ap2;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,20 @@ public class Ap22Application {
 
 	public static void main(String[] args) {
 		try(var context = SpringApplication.run(Ap22Application.class, args)){
-			context.getBean(ImprimeDadosPessoa.class).imprimePessoa();
+
+			//Inicializando os componentes
+			var fileReaderComponent = context.getBean(FileReaderComponent.class);
+			var fileWriterComponent = context.getBean(FileWriterComponent.class);
+			var parserComponent = context.getBean(ParserComponent.class);
+
+			try {
+				String json = fileReaderComponent.read("example_input.json");
+				String csv = parserComponent.jsonToCsv(json);
+				fileWriterComponent.write("output.csv", csv);
+			} catch (IOException e) {
+				System.out.println("Erro ao manipular arquivos: ");
+				e.printStackTrace();
+			}
 		}
 	}
 
